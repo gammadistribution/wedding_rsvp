d38454658ed13e1b937d842ceba8db36bd895710
jetpackjoust <matthew@jetpackjoust.com>
2015-01-12 23:21:56 -0500

    Initial project version

---
+++ wedding_rsvp/settings/common.py
@@ -0,0 +1,217 @@
+"""Common settings and globals."""
+
+import sys
+import os
+from os.path import abspath, basename, dirname, join, normpath
+
+# ########## PATH CONFIGURATION
+
+# Absolute filesystem path to this Django project directory.
+DJANGO_ROOT = dirname(dirname(abspath(__file__)))
+
+# Site name.
+SITE_NAME = basename(DJANGO_ROOT)
+
+# Absolute filesystem path to the top-level project folder.
+SITE_ROOT = dirname(DJANGO_ROOT)
+
+# Absolute filesystem path to the secret file which holds this project's
+# SECRET_KEY. Will be auto-generated the first time this file is interpreted.
+
+SECRET_DIR = normpath(join(SITE_ROOT, 'deploy'))
+
+if not os.path.isdir(SECRET_DIR):
+    os.makedirs(SECRET_DIR)
+
+SECRET_FILE = normpath(join(SECRET_DIR, 'SECRET_FILE'))
+
+
+# Add all necessary filesystem paths to our system path so that we can use
+# python import statements.
+sys.path.append(SITE_ROOT)
+sys.path.append(normpath(join(SITE_ROOT, 'apps')))
+sys.path.append(normpath(join(SITE_ROOT, 'libs')))
+
+
+# ########## END PATH CONFIGURATION
+
+
+# ########## DEBUG CONFIGURATION
+
+# Disable debugging by default.
+DEBUG = False
+TEMPLATE_DEBUG = DEBUG
+
+# ########## END DEBUG CONFIGURATION
+
+
+# ########## MANAGER CONFIGURATION
+
+# Admin and managers for this project. These people receive private site
+# alerts.
+ADMINS = (
+    ("Matthew Tiger", "matthew.c.tiger@gmail.com"),
+)
+
+MANAGERS = ADMINS
+
+# ########## END MANAGER CONFIGURATION
+
+
+# ########## GENERAL CONFIGURATION
+
+# Local time zone for this installation. Choices can be found here:
+# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name although not all
+# choices may be available on all operating systems. On Unix systems, a value
+# of None will cause Django to use the same timezone as the operating system.
+# If running in a Windows environment this must be set to the same as your
+# system time zone.
+TIME_ZONE = 'America/Detroit'
+
+# Language code for this installation. All choices can be found here:
+# http://www.i18nguy.com/unicode/language-identifiers.html.
+LANGUAGE_CODE = 'en-us'
+
+# The ID, as an integer, of the current site in the django_site database table.
+# This is used so that application data can hook into specific site(s) and a
+# single database can manage content for multiple sites.
+SITE_ID = 1
+
+# If you set this to False, Django will make some optimizations so as not
+# to load the internationalization machinery.
+USE_I18N = False
+
+# If you set this to False, Django will not format dates, numbers and
+# calendars according to the current locale.
+USE_L10N = False
+
+# Sets the date format to ISO standards.
+DATE_FORMAT = 'Y-m-d'
+DATETIME_FORMAT = 'Y-m-d H:m:s'
+
+# ########## END GENERAL CONFIGURATION
+
+
+# ########## MEDIA CONFIGURATION
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+MEDIA_ROOT = normpath(join(SITE_ROOT, 'media'))
+
+# URL that handles the media served from MEDIA_ROOT.
+MEDIA_URL = '/media/'
+
+# ########## END MEDIA CONFIGURATION
+
+
+# ########## STATIC FILE CONFIGURATION
+
+# Absolute path to the directory static files should be collected to. Don't put
+# anything in this directory yourself; store your static files in apps' static/
+# subdirectories and in STATICFILES_DIRS.
+STATIC_ROOT = normpath(join(SITE_ROOT, 'static'))
+
+# URL prefix for static files.
+STATIC_URL = '/static/'
+
+# URL prefix for admin static files -- CSS, JavaScript and images.
+ADMIN_MEDIA_PREFIX = '/static/admin/'
+
+# Additional locations of static files.
+STATICFILES_DIRS = (
+    os.path.join(dirname(DJANGO_ROOT), 'templates', 'assets'),
+)
+
+# List of finder classes that know how to find static files in various
+# locations.
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder'
+)
+
+# ########## END STATIC FILE CONFIGURATION
+
+
+# ########## TEMPLATE CONFIGURATION
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader'
+)
+
+# Directories to search when loading templates.
+TEMPLATE_DIRS = (
+    normpath(join(SITE_ROOT, 'templates')),
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.contrib.auth.context_processors.auth",
+    "django.core.context_processors.debug",
+    "django.core.context_processors.i18n",
+    "django.core.context_processors.media",
+    "django.core.context_processors.static",
+    "django.core.context_processors.tz",
+    "django.contrib.messages.context_processors.messages")
+
+# ########## END TEMPLATE CONFIGURATION
+
+
+# ########## MIDDLEWARE CONFIGURATION
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+# ########## END MIDDLEWARE CONFIGURATION
+
+
+# ########## APP CONFIGURATION
+
+INSTALLED_APPS = (
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Admin panel and documentation.
+    'django.contrib.admin',
+    'django.contrib.admindocs'
+)
+
+# ########## END APP CONFIGURATION
+
+
+# ########## URL CONFIGURATION
+
+ROOT_URLCONF = '{}.urls'.format(SITE_NAME)
+
+# ########## END URL CONFIGURATION
+
+
+# ########## KEY CONFIGURATION
+
+# Try to load the SECRET_KEY from our SECRET_FILE. If that fails, then generate
+# a random SECRET_KEY and save it into our SECRET_FILE for future loading. If
+# everything fails, then just raise an exception.
+characters = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+try:
+    with open(SECRET_FILE, 'r') as f:
+        SECRET_KEY = f.read().strip()
+except IOError:
+    try:
+        import random
+        SECRET_KEY = ''.join([random.SystemRandom().choice(characters)
+                              for i in range(50)])
+        with open(SECRET_FILE, 'w') as f:
+            f.write(SECRET_KEY)
+    except IOError:
+        message = 'Cannot open file `{}` for writing.'
+        raise Exception(message.format(SECRET_FILE))
+
+# ########## END KEY CONFIGURATION
---
+++ wedding_rsvp/settings/dev.py
@@ -0,0 +1,73 @@
+"""Development settings and globals."""
+import os
+import sys
+from os.path import dirname, realpath
+
+from jetpackjoust.settings.common import *
+
+
+# ########## DEBUG CONFIGURATION
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+# ########## END DEBUG CONFIGURATION
+
+
+# ########## ALLOWED HOSTS CONFIGURATION
+
+ALLOWED_HOSTS = ['localhost']
+
+# ########## END ALLOWED HOSTS
+
+
+# ######### SITE CONFIGURATION
+
+# primary key from django_site table is 1 for localhost:8000
+
+SITE_ID = 1
+
+# ######## END SITE CONFIGURATION
+
+
+# ########## EMAIL CONFIGURATION
+
+EMAIL_BACKEND = 'django.core.mail.backends.dummy.EmailBackend'
+
+# ########## END EMAIL CONFIGURATION
+
+
+# ########## DATABASE CONFIGURATION
+
+
+def get_db_credentials():
+    """Helper function to grab username and password from relative path
+    ../../../deploy/db_credentials and returns dictionary containing keys
+    "user" and "password", which are the first entries of the lines in file.
+    """
+    location = os.path.join(dirname(dirname(dirname(realpath(__file__)))),
+                            'deploy', 'db_credentials')
+    try:
+        with open(location) as f:
+            lines = [line.strip('\n').split('\t') for line in f.readlines()]
+            credentials = {line[0]: line[1] for line in lines}
+    except(IOError):
+        sys.exit(1)
+    return credentials
+
+
+credentials = get_db_credentials()
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'jpj_content',
+        'USER': credentials['user'],
+        'PASSWORD': credentials['password'],
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+# ########## END DATABASE CONFIGURATION
