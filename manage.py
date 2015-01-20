2762b909e53ab7a3a9154c2e8fed151069d5b7d9
Matthew Tiger <matthew@debian>
2015-01-19 20:48:16 -0500

    For development purposes, set manage.py to use dev settings by default

--- manage.py
+++ manage.py
@@ -2,8 +2,10 @@
 import os
 import sys
 
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wedding_rsvp.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE",
+                          "wedding_rsvp.settings.dev")
 
     from django.core.management import execute_from_command_line
 
