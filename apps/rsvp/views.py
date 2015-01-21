e2a31bad9c384d376390c45bbf97dc7be87d47fd
Matthew Tiger <matthew.c.tiger@gmail.com>
2015-01-20 23:57:26 -0500

    Created form wizard view

--- apps/rsvp/views.py
+++ apps/rsvp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.contrib.formtools.wizard.views import SessionWizardView
 
-# Create your views here.
+
+class RsvpWizard(SessionWizardView):
+    """Class based view for Form Wizard for RsvpAttendanceForm and
+    RsvpPreferenceForm for Rsvp model. Creates page first for
+    RsvpAttendanceForm and if attendance is True, redirect to second page for
+    RsvpPreferenceForm and collect data for rest of model.
+    """
+    def done(self, form_list, **kwargs):
+        print(form_list)
+        return HttpResponseRedirect('')
