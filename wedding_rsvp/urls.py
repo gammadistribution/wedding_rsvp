73a55a179dc11e6b9e028b514b00de64c1c65d5d
Matthew Tiger <matthew.c.tiger@gmail.com>
2015-01-21 00:07:46 -0500

    Included rsvp.urls into main site urls conf

--- wedding_rsvp/urls.py
+++ wedding_rsvp/urls.py
@@ -1,10 +1,21 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 from django.contrib import admin
+admin.autodiscover()
+
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'wedding_rsvp.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^rsvp/', include('rsvp.urls'))
+                       )
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+                            url(r'^media/(?P<path>.*)$',
+                                'django.views.static.serve',
+                                {'document_root': settings.MEDIA_ROOT}),
+                            url(r'^static/(?P<path>.*)$',
+                                'django.views.static.serve',
+                                {'document_root': settings.STATIC_ROOT})
+                            )
