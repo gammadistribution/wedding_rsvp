ce62b62ea6183e3be393f491834589294a6e10e9
Matthew Tiger <matthew.c.tiger@gmail.com>
2015-01-21 22:51:17 -0500

    Added template name to view

--- apps/rsvp/views.py
+++ apps/rsvp/views.py
@@ -2,12 +2,14 @@ from django.http import HttpResponseRedirect
 from django.contrib.formtools.wizard.views import SessionWizardView
 
 
-class RsvpWizard(SessionWizardView):
+class RsvpWizardView(SessionWizardView):
     """Class based view for Form Wizard for RsvpAttendanceForm and
     RsvpPreferenceForm for Rsvp model. Creates page first for
     RsvpAttendanceForm and if attendance is True, redirect to second page for
     RsvpPreferenceForm and collect data for rest of model.
     """
+    template_name = 'rsvp/attendance.html'
+
     def done(self, form_list, **kwargs):
         print(form_list)
-        return HttpResponseRedirect('')
+        return HttpResponseRedirect('redirect-page')
